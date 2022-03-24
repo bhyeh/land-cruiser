576b95840159948288f8bcff48dab8d2f8a24076
Benjamin <by253@cornell.edu>
2022-03-24 12:59:21 -0400

    BATParser - modified 'parse_mileage'

--- BATParser.py
+++ BATParser.py
@@ -6,10 +6,10 @@ import pandas as pd
 class Parser():
     """ A parser for 60 Series Toyota Land Cruiser listings on BringATrailer.com (BAT)
     
-    Methods
+    Method
     -------
     parse(page_source)
-        Parses and retrieves auction and vehicle information from a listing page
+        Parses and retrieves auction and vehicle information from listing page
     
     """
 
@@ -20,7 +20,7 @@ class Parser():
 
         Parameters
         ----------
-        auction_details, auction_stats, auction_essentials: bs4.element.Tag
+        auction_meta_details: (bs4.element.Tag * bs4.element.Tag * bs4.element.Tag)
 
         Returns
         -------
@@ -30,25 +30,25 @@ class Parser():
         auction_details, auction_stats, auction_essentials = auction_meta_details
         # auction details
         price = auction_details.find('span', {'class' : 'info-value noborder-tiny'}).strong.text.split('$')[-1]
-        sell_date = auction_details.find('span', {'class': 'date'}).text.split(' ')[-1]
+        auction_date = auction_details.find('span', {'class': 'date'}).text.split(' ')[-1]
         no_comments = auction_details.find('span', {'class' : 'comments_header_html'}).find('span', {'class' :'info-value'}).text
 
         # auction stats
         no_views, no_watchers = tuple([stat.text.split(' ')[0] for stat in auction_stats.find('td', {'class': 'listing-stats-views'}).find_all('span')])
         no_bids = auction_stats.find('td', {'class': 'listing-stats-value number-bids-value'}).text
         # reserve met
-        reserve_met = True
+        sold = '1'
         bid_result = auction_stats.find('td', {'class' : 'listing-stats-value'}).text.lower()
         bid_result_split = bid_result.split(' ')
         bid_result_split = [substring.strip(' (),') for substring in bid_result_split]
         bid_result = ' '.join(bid_result_split)
         if 'reserve not met' in bid_result:
-            reserve_met = False
+            sold = '0'
 
-        # location
+        # selling location
         seller_location = auction_essentials.find_all('a', href=True)[2].text.lower()
         
-        return price, sell_date, reserve_met, seller_location, no_comments, no_views, no_watchers, no_bids
+        return price, auction_date, sold, seller_location, no_comments, no_views, no_watchers, no_bids
 
     def parse_year(self, title_str):
         """ Parses year
@@ -72,13 +72,24 @@ class Parser():
             idx = title_str.find('199')
             year_str = title_str[idx:idx+4]
         return year_str
+
+    def parse_body_code(self, title_str):
+        """ Parses series
+        
+        """
+
+        code = np.nan
+        if 'j' in title_str:
+            idx = title_str.find('j') - 1
+            code = title_str[idx:idx+4]
+        return code
     
-    def parse_title(self, listing_title):
+    def parse_title(self, title_str):
         """ Parses listing title and returns the year and series code
 
         Parameters
         ----------
-        listing_title: str
+        title_str: str
             listing title that has follows approximate form: '19xx Toyota Land Cruiser yJ6y'
 
         Returns
@@ -86,19 +97,16 @@ class Parser():
         year: str
             year of land cruiser
         series: str
-            code indicating series/generation (e.g., 'fj60', 'fj62', 'hj61')
+            code indicating series/generation (e.g., 'fj60', 'hj60', 'fj62', 'hj61',)
 
         """
 
-        # initialize both year and body code as null
-        code = np.nan
         # lowercase title
-        listing_title = listing_title.lower()
+        title_str = title_str.lower()
         # parse year from title
-        year_str = self.parse_year(listing_title)
+        year_str = self.parse_year(title_str)
         # parse body code
-        code_idx = listing_title.find('j') - 1
-        code = listing_title[code_idx:code_idx+4]
+        code = self.parse_body_code(title_str)
         return year_str, code
 
     def parse_mileage(self, string):
@@ -112,6 +120,7 @@ class Parser():
         
         """
 
+        miles = np.nan
         split = string.split(' ')
         split = [substring.strip(' ()~,') for substring in split]
         string = ' '.join(split)
@@ -121,14 +130,16 @@ class Parser():
         # Format 2: 'xxx.xxx indicated miles' 
         elif 'indicated miles' in string:
             miles = string.split(' indicated miles')[0].split(' ')[-1]
+        # Format 3: 'xxx.xxx reported chassis miles'
+        elif 'reported chassis miles' in string:
+            miles = string.split(' reported chassis miles')[0].split(' ')[-1]
         # Format 3: 'xxx.xxxx miles'
         elif 'miles' in string:
             miles = string.split(' miles')[0].split(' ')[-1]
         # Format 4: 'xxx.xxx shown'
         elif 'shown' in string:
             miles = string.split(' shown')[0].split(' ')[-1]
-        else:
-            miles = np.nan
+
         return miles
 
     def parse_transmission(self, string):
@@ -160,14 +171,6 @@ class Parser():
         
         """
 
-        # no_paint_keywords = sum([1 for keyword in paint_keywords if keyword in s])
-        # no_interior_keywords = sum([1 for keyword in interior_keywords if keyword in s])
-        # # catch case where 'over' keyword misparsing paint description
-        # if ('tank' in s or 'tooth' in s or 'over' in s or 'tone' in s or 'light' in s) & (no_paint_keywords == 1):
-        #     pass
-        # elif (no_paint_keywords >= no_interior_keywords):
-        #     paint = s
-
         pass
     
     def parse_vehicle(self, listing_details):
@@ -190,8 +193,8 @@ class Parser():
                 **Misc. Items**
             ]
 
-            Notes:
-            (1) Descriptions vary tremendously from listing to listing, the exact index occurence of items often change,
+            Note:
+                Descriptions vary tremendously from listing to listing, the exact index occurence of items often change,
                 and items are sometimes not listed. However the overall item structure is predictable. 
                 (E.g., interior description is always after exterior description, transfer is after transmission, etc.)
 
@@ -208,7 +211,7 @@ class Parser():
         listing_details = [spec.lower() for spec in listing_details]
         # keyword occurence to identify details
         mileage_keywords = ['miles', 'shown']
-        engine_keywords = ['-liter', 'inline', 'v8', 'diesel', 'straight']
+        engine_keywords = ['3.4', '4.0', '4.2', '-liter', 'inline', 'v8', 'diesel', 'straight', 'cylinder']
         transmission_keywords = ['manual', 'automatic', 'transmis', 'gear', 'box'] 
         paint_keywords = ['paint', 'exterior', 'metallic', 'finish', 'tone', 'wrap', 'over', 'decal', 
                           'light', 'silver', 'white', 'gray', 'beige', 'brown', 'blue', 'red', 'tan']
@@ -226,7 +229,7 @@ class Parser():
             elif any(keyword in s for keyword in paint_keywords) & (pd.isna(paint)):
                 no_paint_keywords = sum([1 for keyword in paint_keywords if keyword in s])
                 no_interior_keywords = sum([1 for keyword in interior_keywords if keyword in s])
-                # catch case where 'over' keyword misparsing paint description
+                # temporary fix; spaghetti code! 
                 if ('tank' in s or 'tooth' in s or 'over' in s or 'tone' in s or 'light' in s) & (no_paint_keywords == 1):
                     pass
                 elif (no_paint_keywords >= no_interior_keywords):
@@ -252,24 +255,26 @@ class Parser():
         """
         
         soup = BeautifulSoup(page_source, 'lxml')
-        # parse auction meta details
+        # auction meta details
         auction_details = soup.find('div', {'class' : 'listing-available-info'})
         auction_stats = soup.find('div', {'id': 'listing-bid-container'})
         auction_essentials = soup.find('div', {'class' : 'essentials'})
+        # parse meta details
         auction_meta_details = (auction_details, auction_stats, auction_essentials)
-        price, sell_date, reserve_met, seller_location, no_comments, no_views, no_watchers, no_bids = self.parse_auction(auction_meta_details)
+        price, auction_date, sold, seller_location, no_comments, no_views, no_watchers, no_bids = self.parse_auction(auction_meta_details)
         
-        # parse listing title
+        # listing title
         listing_title = soup.find('h1', class_='post-title listing-post-title').text
+        # parse listing title
         year, code = self.parse_title(listing_title)
         
-        # parse listing details
+        # listing details
         listing_details = soup.find('div', class_='essentials')
         listing_specs = [spec.text for spec in listing_details.find('ul').find_all('li')]
+        # parse listing details
         miles, engine, trans, exterior_paint, interior, misc = self.parse_vehicle(listing_specs)
         
         # scraped information
-        scraped_page = [listing_title, price, sell_date, reserve_met, no_views, no_watchers, no_comments, no_bids, 
+        scraped_page = [listing_title, price, auction_date, sold, no_views, no_watchers, no_comments, no_bids, 
                         seller_location, year, code, miles, engine, trans, exterior_paint, interior, misc]
         return scraped_page
-

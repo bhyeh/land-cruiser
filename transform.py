795a47085294a211cd7ba3991afd06ed50662bde
benjamin <by253@cornell.edu>
2022-03-25 18:02:14 -0400

    class transformer

--- transform.py
+++ transform.py
@@ -1,12 +1,32 @@
 import numpy as np
 import pandas as pd
+import re
+from colour import Color
 
-class Transform():
-    """
+class Transformer():
+    """ 
     
+    Method
+    -------
+    transform(df)
+        
     """
 
-    def string_to_int(self, str_num : str):
+
+    def format_date(self, dates: pd.Series) -> pd.Series :
+        """
+
+        Parameters
+        ---------- 
+
+        Returns
+        -------
+
+        """
+
+        pass 
+
+    def string_to_int(self, str_num : str) -> int:
         """ Converts string number representation to integer
 
         Use for transforming price, number of views, watchers, comments, and bids
@@ -25,7 +45,7 @@ class Transform():
         int_num = int(str_num)
         return int_num
     
-    def transform_miles(self, str_miles: str):
+    def transform_miles(self, str_miles: str) -> int:
         """ Parses and transforms 'mileage' to int
 
         Parameters
@@ -50,40 +70,7 @@ class Transform():
 
         return int_miles
 
-    def parse_60series(self, str_engine):
-        """
-        """
-        
-        if '3.4' in str_engine:
-            engine = '3B'
-        elif '4.2' in str_engine:
-            engine = '2F'
-        elif '4.0' in str_engine:
-            engine = '2H'
-
-        return engine
-
-    def parse_61series(self, str_engine):
-        """
-        """
-        
-        if '3.4' in str_engine:
-            engine = '3B'
-        elif '4.0' in str_engine:
-            engine = '12H-T'
-
-        return engine
-
-    def parse_62series(self, str_engine):
-        """
-        """
-
-        if '4.0' in str_engine:
-            engine = '3F-E'
-
-        return engine
-
-    def transform_engine(self, str_engine):
+    def transform_engine(self, df: pd.DataFrame) -> pd.DataFrame:
         """ Normalizes engine description
         
         60
@@ -98,9 +85,170 @@ class Transform():
 
         """
 
+        def parse_60series(str_engine: str) -> str:
+            """
+
+            Parameters
+            ---------- 
+
+            Returns
+            -------
+
+            """
+            
+            # 3B : 3.4-Liter Diesel
+            if '3.4' in str_engine:
+                engine = '3B'
+            # 2F : 4.2-Liter Gasoline
+            elif '4.2' in str_engine:
+                engine = '2F'
+            # 2H : 4.0-Liter Diesel
+            elif '4.0' in str_engine:
+                engine = '2H'
+            # Other(Swapped)
+            else:
+                engine = 'Other(Swapped)'
+
+            return engine
+
+        def parse_61series(str_engine: str) -> str:
+            """
+
+            Parameters
+            ---------- 
+
+            Returns
+            -------
+
+            """
+            
+            # 3B : 3.4-Liter Diesel
+            if '3.4' in str_engine:
+                engine = '3B'
+            # 12H-T : 4.0-Liter Diesel
+            elif '4.0' in str_engine:
+                engine = '12H-T'
+            # Other(Swapped)
+            else:
+                engine = 'Other(Swapped)'
+
+            return engine
+
+        def parse_62series(str_engine: str) -> str:
+            """
+
+            Parameters
+            ---------- 
+
+            Returns
+            -------
+
+            """
+
+            # 3F-E : 4.0-Liter
+            if '4.0' in str_engine:
+                engine = '3F-E'
+            # Other(Swapped)
+            else:
+                engine = 'Other(Swapped)'
+
+            return engine
+
+        # Subset 60 Series
+        subset_60series = np.nan
+
+        # Subset 61 Series
+        subset_61series = np.nan
+
+        # Subset 62 Series
+        subset_62series = np.nan
+
         pass
 
-    def stem_exterior(self, exterior_description):
+    def stem_exterior(self, exterior_description: pd.Series) -> pd.Series:
+        """
+
+        Parameters
+        ---------- 
+
+        Returns
+        -------
+
+        """
+
+        def strip(string: str) -> str:
+            """
+
+            Parameters
+            ---------- 
+
+            Returns
+            -------
+
+            """
+            
+            string = re.sub('-', ' ', string)
+            string = re.sub('/', ' ', string)
+            split = string.split(' ')
+            split = [substring.strip(' -') for substring in split]
+            string = ' '.join(split).strip(' ')
+            return string
+
+        def sub_color(string):
+            """
+
+            Parameters
+            ---------- 
+
+            Returns
+            -------
+
+            """
+            
+            string = re.sub('copper', 'brown', string)
+            string = re.sub('bronze', 'brown', string)
+            string = re.sub('cream', 'tan', string)
+            string = re.sub('creme', 'tan')
+            return string
+
+        def is_color(string):
+            """
+
+            Parameters
+            ---------- 
+
+            Returns
+            -------
+
+            """
+
+            try:
+                Color(string)
+                return True
+            except ValueError:
+                return False
+            
+        def stem_color(string: str) -> str:
+            """
+
+            Parameters
+            ---------- 
+
+            Returns
+            -------
+
+            """
+            
+            color = [substring for substring in string.split(' ') if is_color(substring)]
+            if len(color):
+                color = color[0]
+            else:
+                color = np.nan
+            return color
+
+        pass
+
+    def transform(self) -> pd.DataFrame:
         """
 
         Parameters

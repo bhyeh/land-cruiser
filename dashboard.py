198d20da4670040b2db51936dfca75e4b0fbd8ca
Benjamin <by253@cornell.edu>
2022-04-06 12:42:36 -0400

    dashboarding

--- dashboard.py
+++ dashboard.py
@@ -5,21 +5,74 @@ import pandas as pd
 
 # Load Land Cruiser data
 landcruisers = pd.read_csv('landcruisers-cleaned.csv')
+landcruisers['closing_date'] = pd.to_datetime(landcruisers.closing_date)
 
-# toy df for plotly
-df = pd.DataFrame(landcruisers.series.value_counts()).reset_index()
-fig = px.bar(df, x = 'index', y = 'series')
+# Figure
+fig = px.scatter(landcruisers, x = 'closing_date', y = 'price', 
+                 color = 'result', 
+                 color_discrete_map = {
+                     'sold' : 'light blue',
+                     'reserve not met' : 'orange'
+                 },
+                 labels = {'closing_date' : 'Auction Date',
+                           'price' : 'Closing Bid (USD)',
+                           'result' : 'Auction Result'})
+
+fig.update_traces(showlegend = False,
+                  hovertemplate = '<b>%{hovertext}</b><br>Bid to %{y:$,} on %{x|%m/%d/%Y}',
+                  hovertext = landcruisers.title.values,
+                  name = '')
+
+fig.update_xaxes(
+    dtick = 'M12',
+    tick0 = '2017-01-01',
+    tickformat = '%m/%Y'
+)
+
+fig.update_yaxes(
+    tickformat = '$,.2r'
+)
+
+fig.update_layout(
+    font_family = 'Arial'
+)
 
 # App layout
 app = Dash(__name__)
 
 app.layout = html.Div(children=[
-    html.H1(children='Land Cruisers', style = {'textAlign' : 'center'}),
+    html.H2(children='BAT Land Cruiser Auction Results', style = {'textAlign' : 'left'}),
 
-    html.Div(children='Unique Count by Series', style = {'textAlign' : 'center'}),
+    html.Div(children=[
+        html.Label('Body Style'),
+        dcc.Dropdown(
+            options = ['60', '61', '62'], 
+            placeholder = 'Select Body Style',
+            multi = True,
+            searchable = False
+            ),
+
+        html.Br(),
+        html.Label('Transmission'),
+        dcc.Dropdown(
+            options = ['Manual', 'Automatic'],
+            placeholder = 'Select Transmission Type',
+            multi = True,
+            searchable = False
+        ),
+
+        html.Br(),
+        html.Label('Body Color'),
+        dcc.Dropdown(
+            options = sorted([color.capitalize() for color in landcruisers.exterior.unique()]),
+            placeholder = 'Select Body Color',
+            multi = True,
+            searchable = False
+        )
+    ]),
 
     dcc.Graph(
-        id='example-graph',
+        id='scatter-plot',
         figure=fig
     )
 ])

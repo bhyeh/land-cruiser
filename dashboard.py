66dd87c1a4c129925f3ad50627e1c0f4ec2c3cab
Benjamin <by253@cornell.edu>
2022-04-08 18:26:52 -0400

    dashboard + median prices

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ from dash import Dash, dcc, html, Input, Output
 
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 # Load Land Cruiser data
 landcruisers = pd.read_csv('landcruisers-cleaned.csv')
@@ -50,22 +51,34 @@ app.layout = html.Div(children=[
         id='scatter-plot',
         figure = {},
         style = {'margin-left' : '5%', 'margin-right' : '5%'}
-    )
+    ),
+
+    html.H4(id = 'median-price', children = {}, style = {'textAlign' : 'center'})
 ], style = {'fontFamily' : 'Helvetica'})
 
 # App callback
 # ------------------------------------------------------------------------------
 
 @app.callback(
-    Output(component_id = 'scatter-plot', component_property= 'figure'),
+    Output(component_id = 'median-price', component_property = 'children'),
+    Output(component_id = 'scatter-plot', component_property = 'figure'),
     Input(component_id = 'bodystyle-dropdown', component_property = 'value'),
     Input(component_id = 'transmission-dropdown', component_property = 'value'))
 def update_figure(bodystyle, trans):
 
+    # Lower case user inputs
     if len(trans):
         trans = [str.lower() for str in trans]
 
+    # Subset landcruisers dataframe
     landcruisers_filtered = landcruisers[(landcruisers.trans.isin(trans)) & (landcruisers.body_type.isin(bodystyle))]
+
+    # Median Price Container
+    if landcruisers_filtered.shape[0]:
+        median_price = np.median(landcruisers_filtered.price)
+        container = 'Median Price: ${:,.0f}'.format(median_price)
+    else:
+        container = 'Median Price: '        
 
     fig = px.scatter(landcruisers_filtered, x = 'closing_date', y = 'price', 
                     color = 'result', 
@@ -85,19 +98,23 @@ def update_figure(bodystyle, trans):
     fig.update_xaxes(
         dtick = 'M12',
         tick0 = '2017-01-01',
-        tickformat = '%m/%Y'
+        tickformat = '%m/%Y',
+        title_font_family = 'Helvetica'
+
     )
 
     fig.update_yaxes(
         range = [0, 140000],
-        tickformat = '$,.2r'
+        tickformat = '$,.2r',
+        title_font_family = 'Helvetica'
+
     )
 
     fig.update_layout(
         font_family = 'Helvetica'
     )
 
-    return fig
+    return container, fig
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
